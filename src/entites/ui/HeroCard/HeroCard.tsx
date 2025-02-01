a2e117963fdaaa1a11f7ea1cf6259ebe988c7400
Egor Dovgalev <egor.dovgalev.anthill@gmail.com>
2025-02-01 18:44:07 +0300

    in progress sitch card

--- src/entites/ui/HeroCard/HeroCard.tsx
+++ src/entites/ui/HeroCard/HeroCard.tsx
@@ -7,51 +7,54 @@ import { HeroInfo } from "./components/HeroInfo/HeroInfo";
 
 export const HeroCard = () => {
   return (
-    <div className={s.card}>
-      <HeroInfo />
-      <div className="flex flex-col gap-[3px]">
-        <HeroAbilityCard
-          type="Vanguard"
-          textAbility={
-            <>
-              <strong>Перехват.</strong> Нанесите 2 урона герою, который выбрал
-              этого героя как цель своеё атаки
-            </>
-          }
-          iconsAbility={<>icons</>}
-        />
-        <HeroAbilityCard
-          type="Flank"
-          textAbility={
-            <>
-              <strong>Перехват.</strong> Нанесите 2 урона герою, который выбрал
-              этого героя как цель своеё атаки
-            </>
-          }
-          iconsAbility={<>icons</>}
-        />
-        <HeroAbilityCard
-          type="Rear"
-          textAbility={
-            <>
-              <strong>Перехват.</strong> Нанесите 2 урона герою, который выбрал
-              этого героя как цель своеё атаки
-            </>
-          }
-          iconsAbility={<>icons</>}
-        />
-        <HeroAbilityCard
-          type="Order"
-          textAbility={
-            <>
-              <strong>Перехват.</strong> Нанесите 2 урона герою, который выбрал
-              этого героя как цель своеё атаки
-            </>
-          }
-          iconsAbility={<>icons</>}
-        />
+    <div className={s.wrapper}>
+      <div className={s.card}>
+        <HeroInfo />
+        <div className="flex flex-col gap-[3px]">
+          <HeroAbilityCard
+            type="Vanguard"
+            textAbility={
+              <>
+                <strong>Перехват.</strong> Нанесите 2 урона герою, который
+                выбрал этого героя как цель своеё атаки
+              </>
+            }
+            iconsAbility={<>icons</>}
+          />
+          <HeroAbilityCard
+            type="Flank"
+            textAbility={
+              <>
+                <strong>Перехват.</strong> Нанесите 2 урона герою, который
+                выбрал этого героя как цель своеё атаки
+              </>
+            }
+            iconsAbility={<>icons</>}
+          />
+          <HeroAbilityCard
+            type="Rear"
+            textAbility={
+              <>
+                <strong>Перехват.</strong> Нанесите 2 урона герою, который
+                выбрал этого героя как цель своеё атаки
+              </>
+            }
+            iconsAbility={<>icons</>}
+          />
+          <HeroAbilityCard
+            type="Order"
+            textAbility={
+              <>
+                <strong>Перехват.</strong> Нанесите 2 урона герою, который
+                выбрал этого героя как цель своеё атаки
+              </>
+            }
+            iconsAbility={<>icons</>}
+          />
+        </div>
+        <LeaderInfo />
       </div>
-      <LeaderInfo />
+      <div className={s.backCard}></div>
     </div>
   );
 };
